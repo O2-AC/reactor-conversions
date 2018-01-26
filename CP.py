3f66464eea66709efe669f7243f2d9dd09b90473
Ole <osterthun.ole@gmail.com>
2018-01-26 18:27:25 +0100

    Update CP.py and CSTR-PFR.py

--- CP.py
+++ CP.py
@@ -1,31 +1,31 @@
-from __future__ import division
+# reaction first order
 import numpy as np
 
 cA0 = 1
-k = 0.2
-tPFR = 1
-tCSTR = 10
+k = 1
+tPFR = 10
+tCSTR = 1
 
 # CSTR -> PFR
 print("----\nCSTR-PFR\n----")
 # concentration behind CSTR
 cAi = cA0/(k * tCSTR + 1)
 convint = ((1-cAi)/cA0) * 100
-print("conversion CSTR: {}".format(convint))
+print("conversion CSTR: {}".format(round(convint, 2)))
 # concentration behind PFR
 cA1 = cAi * np.exp(-k * tPFR)
 conversion = ((1-cA1)/cA0)*100
-print("conversion PFR: {}".format(conversion - convint))
-print("conversion: {}".format(conversion))
+print("conversion PFR: {}".format(round(conversion - convint, 2)))
+print("conversion: {}".format(round(conversion, 2)))
 # PFR -> CSTR
 print("----\nPFR-CSTR\n----")
 # concentration behind PFR
 cAi = cA0 * np.exp(-k *tPFR)
 convint = ((1-cAi)/cA0) *100
-print("conversion PFR: {}".format(convint))
+print("conversion PFR: {}".format(round(convint, 2)))
 
 # concentration behind CSTR
 cA2 = cAi/(k * tCSTR + 1)
 conversion = ((1-cA1)/cA0)*100
-print("conversion CSTR: {}".format(conversion - convint))
-print("conversion: {}".format(conversion))
+print("conversion CSTR: {}".format(round(conversion - convint, 2)))
+print("conversion: {}".format(round(conversion, 2)))
--- CSTR-PFR.py
+++ CSTR-PFR.py
@@ -1,35 +1,31 @@
 # reaction 2nd order
-# k: 1
-# fixed conversions in first reactor
-# therefore: given residence times for first reactors useless
-# if Conversions are not fixed for first reactor PFR -> CSTR will lead to higher conversions
 import numpy as np
 
-k = 1
+k = 10
 cA0 = 1
-n = 1
+n = 0.1
 
-t1 = 100 # first reactor
-t2 = n * t1 # second reactor
-cAi1 = 0.5 # (np.sqrt(1 + 4 * t1 * k * cA0) - 1)/(2 * t1 * k)
+tCSTR = 1 # first reactor
+tPFR = n * tCSTR # second reactor
+cAi1 = (np.sqrt(1 + 4 * tCSTR * k * cA0) - 1)/(2 * tCSTR * k)
 convint1 = ((1-cAi1)/1)*100
-cA1 = (1)/((1/cAi1) + t2 * k)
+cA1 = (1)/((1/cAi1) + tPFR * k)
 conversion1 = ((1-cA1)/1)*100
-cAi2 = 0.5 # (1)/((1/cA0) + t1 * k)
+cAi2 = (1)/((1/cA0) + tPFR * k)
 convint2 = ((1-cAi2)/1)*100
-cA2 = (np.sqrt(1 + 4 * t2 * k * cAi2) - 1)/(2 * t2 * k)
+cA2 = (np.sqrt(1 + 4 * tCSTR * k * cAi2) - 1)/(2 * tCSTR * k)
 conversion2 = ((1-cA2)/1)*100
 print("-----\nCSTR -> PFR\n-----")
-print("residence time CSTR: {}".format(t1))
-print("residence time PFR: {}".format(t2))
+print("residence time CSTR: {}".format(tCSTR))
+print("residence time PFR: {}".format(tPFR))
 print("concentration after CSTR: {}".format(round(cAi1, 2)))
 print("conversion in CSTR: {}".format(round(convint1, 2)))
 print("concentration after PFR: {}".format(round(cA1, 2)))
 print("conversion in PFR: {}".format(round(conversion1-convint1, 2)))
 print("overall conversion: {}".format(round(conversion1, 2)))
 print("-----\nPFR -> CSTR\n-----")
-print("residence time CSTR: {}".format(t2))
-print("residence time PFR: {}".format(t1))
+print("residence time CSTR: {}".format(tCSTR))
+print("residence time PFR: {}".format(tPFR))
 print("concentration after PFR: {}".format(round(cAi2, 2)))
 print("conversion in PFR: {}".format(round(convint2, 2)))
 print("concentration after CSTR: {}".format(round(cA2, 2)))
